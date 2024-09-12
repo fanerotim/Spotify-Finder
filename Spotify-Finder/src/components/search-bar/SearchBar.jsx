f22a15e1d3b5c703a1ef0f505e025fa89de5eea5
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-12 14:14:04 +0100

    create search bar component

---
+++ Spotify-Finder/src/components/search-bar/SearchBar.jsx
@@ -0,0 +1,11 @@
+const SearchBar = () => {
+    return (
+        <>
+            <input 
+            type="text" 
+            placeholder='Search artist or album'/>
+        </>
+    )
+}
+
+export default SearchBar;
