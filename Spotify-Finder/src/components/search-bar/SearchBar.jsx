6c23ce2a4e703323460090ad834e57f00ed37412
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-12 14:16:28 +0100

    install sass

--- Spotify-Finder/src/components/search-bar/SearchBar.jsx
+++ Spotify-Finder/src/components/search-bar/SearchBar.jsx
@@ -1,9 +1,13 @@
+import './SearchBar.scss'
+
 const SearchBar = () => {
     return (
         <>
             <input 
             type="text" 
-            placeholder='Search artist or album'/>
+            placeholder='Search artist or album'
+            className="search__input"
+            />
         </>
     )
 }
