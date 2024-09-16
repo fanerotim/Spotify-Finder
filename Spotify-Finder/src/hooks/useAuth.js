e526ed561c2db47ba330ac780735c6d42c63107a
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-16 14:12:59 +0100

    adjust hook

--- Spotify-Finder/src/hooks/useAuth.js
+++ Spotify-Finder/src/hooks/useAuth.js
@@ -1,11 +1,7 @@
-import { useState } from "react";
-
 const useAuth = () => {
 
-    const [token, setToken] = useState(null)
-
     const getAccessToken = () => {
-        fetch('https://accounts.spotify.com/api/token', {
+        const token = fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             body: new URLSearchParams({
                 'grant_type': 'client_credentials'
@@ -15,13 +11,11 @@ const useAuth = () => {
                 'Authorization': 'Basic ' + btoa(import.meta.env.VITE_CLIENT_ID + ':' + import.meta.env.VITE_CLIENT_SECRET)
             }
         })
-        .then(res => res.json())
-        .then(result => setToken(result.access_token))
 
         return token;
     }
 
-    return { getAccessToken, token }
+    return { getAccessToken }
 }
 
 export default useAuth;
