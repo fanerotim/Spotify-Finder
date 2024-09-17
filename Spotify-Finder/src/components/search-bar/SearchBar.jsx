63d1b5709f9d0964bf89fe80512c4a4d1f6f90d3
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-17 14:25:59 +0100

    add functionality to input component

--- Spotify-Finder/src/components/search-bar/SearchBar.jsx
+++ Spotify-Finder/src/components/search-bar/SearchBar.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from 'react';
 import './SearchBar.scss'
+import useAuth from '../../hooks/useAuth';
 
 const SearchBar = () => {
+
+    const [token, setToken] = useState(null)
+    const { getAccessToken } = useAuth();
+
+    const [input, setInput] = useState('');
+
     return (
         <div className='search__container'>
             <h1 className='search__container--logo'>Spotify Finder</h1>
@@ -8,6 +16,8 @@ const SearchBar = () => {
                 type="text"
                 placeholder='Search artist or album'
                 className="search__container--input"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
             />
         </div>
     )
