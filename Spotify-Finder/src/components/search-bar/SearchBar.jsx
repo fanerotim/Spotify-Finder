2b2cd64ef27622f8cdb026cc4ffa9574468c4e6b
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-18 14:18:44 +0100

    testing api call to get certain album / artist

--- Spotify-Finder/src/components/search-bar/SearchBar.jsx
+++ Spotify-Finder/src/components/search-bar/SearchBar.jsx
@@ -6,8 +6,21 @@ const SearchBar = () => {
 
     const [token, setToken] = useState(null)
     const { getAccessToken } = useAuth();
-
     const [input, setInput] = useState('');
+
+    // complete the api call for certain artist/ album 
+    useEffect(() => {
+        (async () => {
+            const token = await getAccessToken();
+            setToken(() => token.access_token)
+
+            const search = fetch('https://api.spotify.com/v1/search?q=2pac', {
+                headers: {
+                    Authorization: 'Bearer ' + token.access_token
+                }
+            })
+        })()
+    }, [])
 
     return (
         <div className='search__container'>
