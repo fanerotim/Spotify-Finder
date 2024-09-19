f11bb3aae592618b52e339f37226b65f504679fb
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-19 14:17:10 +0100

    set up initial card markup

--- Spotify-Finder/src/components/card/Card.jsx
+++ Spotify-Finder/src/components/card/Card.jsx
@@ -1,10 +1,21 @@
 import './Card.scss'
 
 const Card = () => {
-    return (
-        <>
 
-        </>
+    const cardCount = [1, 1, 1, 1];
+
+    return (
+        <div className='card__container'>
+            
+            {cardCount.map(card => (
+                <section className='card'>
+                    <img src="" alt="" />
+                    <h1>Album name</h1>
+                </section>
+            ))}
+
+        </div>
+
     )
 }
 
