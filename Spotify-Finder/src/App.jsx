7a26976a22dfca599e9d0ee21962d5fb0a2d87c0
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-22 20:47:47 +0100

    remove unnecessary code

--- Spotify-Finder/src/App.jsx
+++ Spotify-Finder/src/App.jsx
@@ -1,12 +1,8 @@
 
 import './App.css'
-import Card from './components/card/Card';
 import SearchBar from './components/search-bar/SearchBar';
 
 function App() {
-
-  const clientID = import.meta.env.VITE_CLIENT_ID;
-  const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
   return (
     <>
