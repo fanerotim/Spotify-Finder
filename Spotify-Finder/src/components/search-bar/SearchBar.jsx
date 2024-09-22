324090d43d97605de5799ec98dfcb36dc61db731
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-22 22:28:03 +0100

    improved handleChange function and left descriptive comments

--- Spotify-Finder/src/components/search-bar/SearchBar.jsx
+++ Spotify-Finder/src/components/search-bar/SearchBar.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from 'react';
 import './SearchBar.scss'
-import useAuth from '../../hooks/useAuth';
+import useFetchToken from '../../hooks/useFetchToken';
 import Card from '../card/Card';
+import useFetchSearch from '../../hooks/useFetchSearch';
 
 const SearchBar = () => {
 
+    // set access token
     const [token, setToken] = useState(null)
-    const { getAccessToken } = useAuth();
+    
+    // import function that calls Spotify API to request access token
+    const { getAccessToken } = useFetchToken();
+
+    // state of user input
     const [input, setInput] = useState('');
+
+    // state for result of user search
     const [albums, setAlbums] = useState([]);
 
-    // complete the api call for certain artist/ album 
+    // on initial render of component get access token and set it in state
     useEffect(() => {
         (async () => {
             const curToken = await getAccessToken();
@@ -18,43 +26,43 @@ const SearchBar = () => {
         })()
     }, [])
 
+    const {search} = useFetchSearch();
+
     const handleChange = async (e) => {
-        setInput(() => e.target.value)
-    }
+        // TODO: When search field is cleaned quickly last result is displayed. Check why this happens and resolve
+        let emptyInput = e.target.value;
+        setInput(prev => e.target.value);
+        
+        if (emptyInput === '') {
+            setAlbums(prev => [])
+            return;
+        }
+        
+        const url = `https://api.spotify.com/v1/search?q=${input}&type=album`;
+        
+        try {
+            const result = await search(url, token);
 
-    useEffect(() => {
-        (async (e) => {
- 
-        const search = await fetch(`https://api.spotify.com/v1/search?q=${input}&type=album`, {
-            headers: {
-                Authorization: 'Bearer ' + token
+            if (result.albums.items) {
+                setAlbums(prev => result.albums.items)
             }
-        })
-
-        if (input === '') {
-            setAlbums([])
+        } catch(err) {
+            console.log(err, 'boom')
         }
-
-        let searchResult = await search.json();
-
-        if (searchResult.albums.items) {
-            setAlbums(prev => searchResult.albums.items)
-        }
-        })()
-    }, [input])
+    }
 
     return (
         <>
-        <div className='search__container'>
-            <h1 className='search__container--logo'>Spotify Finder</h1>
-            <input
-                type="text"
-                placeholder='Search artist or album'
-                className="search__container--input"
-                value={input}
-                onChange={(e) => handleChange(e)}
-            />
-        </div>
+            <div className='search__container'>
+                <h1 className='search__container--logo'>Spotify Finder</h1>
+                <input
+                    type="text"
+                    placeholder='Search artist or album'
+                    className="search__container--input"
+                    value={input}
+                    onChange={(e) => handleChange(e)}
+                />
+            </div>
             {albums.length > 0 && <Card albums={albums} />}
         </>
     )
