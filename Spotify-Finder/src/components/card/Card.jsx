7127ea10626eacbb8b6bd8c9130bc21bae2679c1
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-25 14:24:46 +0100

    set up each card to link to separate page for which album details component will be used

--- Spotify-Finder/src/components/card/Card.jsx
+++ Spotify-Finder/src/components/card/Card.jsx
@@ -1,19 +1,22 @@
-import './Card.scss'
+import './Card.scss';
+import {Link} from 'react-router-dom'
 
 const Card = ({ albums }) => {
 
     return (
         <div className='card__container'>
             {albums.map(album => (
-                <div className='card__wrapper'>
-                    <section key={album.id} className='image__wrapper'>
-                        <img className='image__wrapper__img' src={album.images[1].url} alt="" />
-                    </section>
+                <div className='card__wrapper' key={album.id}>
+                    <Link to={`albums/${album.id}`}>
+                        <section className='image__wrapper'>
+                            <img className='image__wrapper__img' src={album.images[1].url} alt="" />
+                        </section>
 
-                    <section className='text__wrapper'>
-                        <p className='text__wrapper__label'>Album</p>
-                        <h1 className='text__wrapper__text'>{album.name}</h1>
-                    </section>
+                        <section className='text__wrapper'>
+                            <p className='text__wrapper__label'>Album</p>
+                            <h1 className='text__wrapper__text'>{album.name}</h1>
+                        </section>
+                        </Link>
                 </div>
             ))}
 
