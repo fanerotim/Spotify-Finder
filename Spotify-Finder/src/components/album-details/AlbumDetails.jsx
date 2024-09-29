30b72d35fa19914b921b3c665d58abebec170029
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-29 18:58:03 +0100

    first iteration of album details functionality
    this page will show all tracks of an album

--- Spotify-Finder/src/components/album-details/AlbumDetails.jsx
+++ Spotify-Finder/src/components/album-details/AlbumDetails.jsx
@@ -1,11 +1,38 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const AlbumDetails = () => {
 
-    const {albumId} = useParams();
+    const { albumId } = useParams();
+
+    const url = `https://api.spotify.com/v1/albums/${albumId}/tracks`
+    const token = localStorage.getItem('access_token');
+
+    const [tracks, setTracks] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const albumTracksReq = await fetch(url, {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            });
+            const albumTracks = await albumTracksReq.json();
+            setTracks(prev => albumTracks.items)
+        })()
+    }, [])
+
+
 
     return (
-        <h1>Hello</h1>
+        <>
+            <h1>Hello, please see the tracks in this album below:</h1>
+            <ul>
+            {tracks.length > 0 && tracks.map(track => (
+                <li>{track.name}</li>
+            ))}
+            </ul>
+        </>
     )
 }
 
