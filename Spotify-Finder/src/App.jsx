ab8727977b844262dfd58329c5c69d5a50f98d37
Petar Cholakov <petar.cholakov30@gmail.com>
2024-09-29 17:47:23 +0100

    added routing

--- Spotify-Finder/src/App.jsx
+++ Spotify-Finder/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
 import SearchBar from './components/search-bar/SearchBar';
+import AlbumDetails from './components/album-details/AlbumDetails';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <SearchBar/>
+    <Routes>
+      <Route path='/' element={<SearchBar/>}></Route>
+      <Route path='/albums/:albumId' element={<AlbumDetails/>}></Route>
+    </Routes>
+
     </>
 
   )
