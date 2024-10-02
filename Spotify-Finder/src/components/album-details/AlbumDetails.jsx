904563da9c151522e57bc5f1d66232cbeaac54d6
Petar Cholakov <petar.cholakov30@gmail.com>
2024-10-02 14:24:20 +0100

    apply styles to details page and test ways to pass props to Link through state object

--- Spotify-Finder/src/components/album-details/AlbumDetails.jsx
+++ Spotify-Finder/src/components/album-details/AlbumDetails.jsx
@@ -2,9 +2,10 @@ import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import './AlbumDetails.scss';
 
-const AlbumDetails = () => {
+const AlbumDetails = (props) => {
     const { albumId } = useParams();
 
+    console.log(props)
     const url = `https://api.spotify.com/v1/albums/${albumId}/tracks`
     const token = localStorage.getItem('access_token');
 
@@ -12,27 +13,32 @@ const AlbumDetails = () => {
 
     useEffect(() => {
         (async () => {
-            const albumTracksReq = await fetch(url, {
-                headers: {
-                    Authorization: 'Bearer ' + token
-                }
-            });
-            const albumTracks = await albumTracksReq.json();
-            setTracks(prev => albumTracks.items)
+
+            try {
+                const albumTracksReq = await fetch(url, {
+                    headers: {
+                        Authorization: 'Bearer ' + token
+                    }
+                });
+                const albumTracks = await albumTracksReq.json();
+                // console.log(albumTracks)
+                setTracks(prev => albumTracks.items)
+            } catch (err) {
+                console.log(err);
+            }
         })()
     }, [])
 
-
     return (
-        <>  
+        <div className="details__container">
             <Link to='/' className="back__button"><span className="material-symbols-outlined">arrow_back</span>Back to homepage</Link>
             <h1 className="track__list__container__heading">Tracklist:</h1>
             <ul className="track__list__container">
-            {tracks.length > 0 && tracks.map((track, index) => (
-                <li className="track__list--item">{index + 1}. {track.name}</li>
-            ))}
+                {tracks.length > 0 && tracks.map((track, index) => (
+                    <li key={track.id} className="track__list--item">{index + 1}. {track.name}</li>
+                ))}
             </ul>
-        </>
+        </div>
     )
 }
 
